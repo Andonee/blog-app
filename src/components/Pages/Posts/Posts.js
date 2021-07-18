6c412f1124d1a34ad5e283795b3b6ed863c01b60
mpoprawa <m.poprawa@smartgeomatic.pl>
2021-07-18 12:47:49 +0200

    add articles page

--- src/components/Pages/Posts/Posts.js
+++ src/components/Pages/Posts/Posts.js
@@ -1,7 +1,127 @@
 import React from 'react'
+import { Card, Input } from '../../UI'
 
 const Posts = () => {
-	return <div>Posts</div>
+	return (
+		<div>
+			<div>
+				<Input type='text' placeholder='search' />
+			</div>
+			<div>
+				{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>{' '}
+				<Card
+					img='https://via.placeholder.com/400x250'
+					title='some title'
+					id='1'
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default Posts
