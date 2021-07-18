59acf41321efb052872614126e187fe5f8ae2942
mpoprawa <m.poprawa@smartgeomatic.pl>
2021-07-18 15:09:55 +0200

    add contact page

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import './Contact.scss'
+import { Image } from '../../UI'
+import contact from '../../../assets/images/contact.jpg'
 
 const Contact = () => {
-	return <div>Contact</div>
+	return (
+		<div className='contact'>
+			<div className='contact_image'>
+				<img src={contact} alt='Contact us' className='contact_image' />
+			</div>
+			<div className='contact_content'>
+				Nunc eget tempor enim. Suspendisse dignissim enim turpis, non lobortis
+				mauris elementum ut. Fusce vitae massa neque. Curabitur lobortis
+				elementum ligula, et vehicula erat condimentum eu. Proin mattis turpis
+				ut nibh viverra, at pretium nunc accumsan. Vestibulum ac risus libero.{' '}
+			</div>
+		</div>
+	)
 }
 
 export default Contact
