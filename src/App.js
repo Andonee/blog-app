b520782e6ef81658616cc39ba1bf8ec7bf442a2b
mpoprawa <m.poprawa@smartgeomatic.pl>
2021-07-18 10:50:39 +0200

    add nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import styles from './App.module.scss'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Pages/Home/Home'
+import Posts from './components/Pages/Posts/Posts'
+import Contact from './components/Pages/Contact/Contact'
 
 const App = () => {
-	return <div className={styles.app}></div>
+	return (
+		<BrowserRouter>
+			<div className={styles.app}>
+				<Navbar />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/articles' component={Posts} />
+					<Route exact path='/contact' component={Contact} />
+					{/* TODO
+					ADD 404 page */}
+				</Switch>
+			</div>
+		</BrowserRouter>
+	)
 }
 
 export default App
