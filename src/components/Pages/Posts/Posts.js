28275b22924f798fad3dc10a04806acf812375c3
mpoprawa <m.poprawa@smartgeomatic.pl>
2021-07-19 15:05:01 +0200

    add mobile styles to articles page

--- src/components/Pages/Posts/Posts.js
+++ src/components/Pages/Posts/Posts.js
@@ -1,119 +1,119 @@
 import React from 'react'
+import styles from './Posts.module.scss'
 import { Card, Input } from '../../UI'
 
 const Posts = () => {
 	return (
-		<div>
-			<div>
+		<div className={styles.posts}>
+			<div className={styles.posts_search}>
 				<Input type='text' placeholder='search' />
 			</div>
-			<div>
-				{' '}
+			<div className={`${styles.posts_list} grid`}>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
 					id='1'
-				/>{' '}
+				/>
 				<Card
 					img='https://via.placeholder.com/400x250'
 					title='some title'
