546700faede246761b27f5b1103936ba1e1be2fc
mpoprawa <m.poprawa@smartgeomatic.pl>
2021-07-20 15:53:53 +0200

    add errorBoundary

---
+++ src/components/UI/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import styles from './ErrorBoundary.module.scss'
+
+class ErrorBoundary extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { hasError: false }
+	}
+
+	static getDerivedStateFromError(error) {
+		return { hasError: true }
+	}
+
+	componentDidCatch(error, info) {
+		console.log('error', error, 'info', info)
+	}
+	render() {
+		if (this.state.hasError) {
+			return <div className={styles.errorBoundary}>Something went wrong :(</div>
+		}
+
+		return this.props.children
+	}
+}
+
+export default ErrorBoundary
